41c1e74497515b4b1f92fdf4f562bdcfc990fff2
Дарья Конева <Tyuiu.KonevaDD@outlook.com>
2024-12-07 15:43:05 +0500

    Task1

--- Tyuiu.KonevDD.Sprint6.Task1.V28.Lib/DataService.cs
+++ Tyuiu.KonevDD.Sprint6.Task1.V28.Lib/DataService.cs
@@ -1,20 +1,31 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint6;
-namespace Tyuiu.KonevaDD.Sprint6.Task1.V28.Lib;
-
-public class DataService : ISprint6Task1V28
+namespace Tyuiu.KonevaDD.Sprint6.Task1.V28.Lib
 {
-    public double[] GetMassFunction(int startValue, int stopValue)
+    public class DataService : ISprint6Task1V28
     {
-        int len = Math.Abs(startValue) + stopValue + 1;
-
-        double[] result = new double[len];
-        int count = 0;
-        for (int i = startValue; i <= stopValue; i++)
+        public double[] GetMassFunction(int startValue, int stopValue)
         {
-            result[count] = Math.Round((Math.Cos(i) + (4 * i) / (2) - Math.Sin(i) * 3 * i), 2);
+            int len = Math.Abs(startValue) + stopValue + 1;
 
-            count++;
+            double[] result = new double[len];
+            int count = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                result[count] = Math.Round((Math.Cos(i) + (4 * i) / (2) - Math.Sin(i) * 3 * i), 2);
+
+                count++;
+            }
+            return result;
         }
-        return result;
     }
+
+
+
+
+
+
+
+
 }
+
+

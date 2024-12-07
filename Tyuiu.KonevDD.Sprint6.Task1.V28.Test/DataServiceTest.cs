c016928530ca85de9495e7ca384f270e8dcbaf26
Дарья Конева <Tyuiu.KonevaDD@outlook.com>
2024-12-07 15:34:31 +0500

    Task1

---
+++ Tyuiu.KonevDD.Sprint6.Task1.V28.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.KonevDD.Sprint6.Task1.V28.Lib;
+namespace Tyuiu.KonevDD.Sprint6.Task1.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double[] res = ds.GetMassFunction(-5, 5);
+            double result = res[0];
+            Assert.AreEqual(4.67, result);
+        }
+    }
+}

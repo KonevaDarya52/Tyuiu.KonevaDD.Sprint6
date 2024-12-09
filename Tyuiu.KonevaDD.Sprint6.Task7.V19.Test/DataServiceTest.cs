22f132ac70c140a6108c53da79c4bd3a20464177
Дарья Конева <Tyuiu.KonevaDD@outlook.com>
2024-12-09 20:59:29 +0500

    Task7

---
+++ Tyuiu.KonevaDD.Sprint6.Task7.V19.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.KonevaDD.Sprint6.Task7.V19.Lib;
+namespace Tyuiu.KonevaDD.Sprint6.Task7.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.KonevaDD.Sprint6.Task7.V19/FormAbout.Designer.cs
@@ -0,0 +1,82 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Tyuiu.KonevaDD.Sprint6.Task7.V19
+{
+    partial class FormAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            buttonClose = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(14, 16);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(228, 265);
+            textBox1.TabIndex = 0;
+            // 
+            // buttonClose
+            // 
+            buttonClose.Location = new Point(157, 301);
+            buttonClose.Margin = new Padding(3, 4, 3, 4);
+            buttonClose.Name = "buttonClose";
+            buttonClose.Size = new Size(86, 31);
+            buttonClose.TabIndex = 1;
+            buttonClose.Text = "Ок";
+            buttonClose.UseVisualStyleBackColor = true;
+            buttonClose.Click += buttonClose_Click;
+            // 
+            // FormAbout
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(256, 348);
+            Controls.Add(buttonClose);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(3, 4, 3, 4);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FormAbout";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "О программе";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button buttonClose;
+    }
+}
